eb4b0f758560e733a804ab04e2f244bd9d0864d2
Shahbaz64 <muhammadshahbazkhawaja@gmail.com>
2021-12-09 01:09:52 +0500

    Fixed Some Issues

---
+++ src/components/calculator.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Buttons from "components/buttons";
+import "styles/calculator.css";
+
+/* eslint-disable no-eval */
+
+class Calculator extends Component {
+  state = {
+    exp: "",
+    res: "",
+  };
+  onClickHandler = (val) => {
+    if (val === "C") {
+      this.setState({
+        exp: "",
+        res: "",
+      });
+    } else if (val === "=") {
+      try {
+        if (!this.state.exp) {
+          this.setState({
+            res: "",
+          });
+        } else {
+          this.setState({
+            res: eval(this.state.exp),
+          });
+        }
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          this.setState({
+            res: "Invalid Input",
+          });
+        }
+      }
+    } else {
+      this.setState({
+        exp: this.state.exp + val,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="calculator-box">
+        <div className="in-out-box">
+          <p className="exp">{this.state.exp}</p>
+          <p className="out">{this.state.res}</p>
+        </div>
+        <Buttons onClickHandler={this.onClickHandler} />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
